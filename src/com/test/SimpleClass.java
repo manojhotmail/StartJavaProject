a616894d820f7a8e09a8a2f6ec772f1b5107aedd
Manoj Lonar <manoj.lonar@lenskart.in>
2018-06-24 10:42:03 +0530

    Added Simple class

---
+++ src/com/test/SimpleClass.java
@@ -0,0 +1,28 @@
+package com.test;
+
+public class SimpleClass {
+	
+	static int width;
+	static int depth;
+	static int height;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		add();
+		
+	}
+	
+	public static void add() {
+		
+		width = 10;
+		depth = 5;
+		height = 9;
+		
+		System.out.println(width + height + depth);
+	
+	}
+	
+	
+
+}
